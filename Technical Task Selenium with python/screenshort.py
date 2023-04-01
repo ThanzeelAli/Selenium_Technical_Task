c61190df870be1e0f84ee34817720df88cb5db05
ThanzeelAli <127336542+ThanzeelAli@users.noreply.github.com>
2023-04-01 21:39:50 +0530

    Update screenshort.py

--- Technical Task Selenium with python/screenshort.py
+++ Technical Task Selenium with python/screenshort.py
@@ -5,12 +5,14 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome("C:\chromedriver_win32 (2)\chromedriver.exe")
 time.sleep(5)
-driver.get("https://web.whatsapp.com/")
+driver.get("https://www.facebook.com/")
 
 #screenshot
 
-element=driver.find_element(By.XPATH,"//span[normalize-space()='Add Files']")
-element.screenshot("upload.png")
+element=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a")
+element.click()
+time.sleep(5)
+element.screenshot("account.png")
 
 driver.implicitly_wait(5)
 
